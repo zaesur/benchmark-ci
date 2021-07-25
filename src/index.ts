f8d97fcba0dc80178d9e2996820c8abccb15f226
Florian Van Belleghem <fvbe@stylelabs.com>
2021-07-25 19:19:15 +0200

    Add env variables

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ import App from "./app";
 import BenchmarkController from "./benchmark/benchmark.controller";
 import BenchmarkService from "./benchmark/benchmark.service";
 
-const redis = new RedisClient({ port: 6380, host: "127.0.0.1" });
+const port: number = parseInt(process.env.PORT || "6379", 10);
+const host: string = process.env.HOST || "127.0.0.1"
+const password: string = process.env.PASSWORD || "";
+
+const redis = new RedisClient({ port, host, password });
 const benchmarkService = new BenchmarkService(redis);
 
 const app = new App([
